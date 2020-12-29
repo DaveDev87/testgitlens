8c37eb6bdb3880e62ab2f03be801e8eabbf90fc6
David Rueda <alexruedarivas@outlook.com>
2020-12-28 23:59:22 -0600

    tercer commit

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 let saludo = "Hola Mundo";
 let mision = 25;
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // console.log(saludo);
 console.log(mision);
+
+for (let index = 0; index < array.length; index++) {
+  console.log(array[index]);
+}
