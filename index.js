de12a81f3430e238ed5329fd83253dfc14968bd0
David Rueda <alexruedarivas@outlook.com>
2020-12-29 17:26:56 -0600

    cambios sin importancia

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 //@ts-check
 
 let saludo = "Hola Mundo";
-let mision = 25;
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let rango = 25;
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // console.log(saludo);
-console.log(mision);
+console.log(rango);
 
-for (let index = 0; index < array.length; index++) {
-  console.log(array[index]);
+// for (let index = 0; index < array.length; index++) {
+//   console.log(array[index]);
+// }
+
+let i = 0;
+while (i <= rango) {
+  console.log(i);
+  i++;
 }
 
 //JetBrains is on fire!!!
